1918a63c00e8591a6f1ad585da1915c8e06e9909
Danikileitor <focusyi@hotmail.com>
2022-12-17 20:02:26 +0100

    empezando

--- web/js/main.js
+++ web/js/main.js
@@ -5,3 +5,29 @@ canvas.width = 1024
 canvas.height = 576
 
 c.fillRect(0, 0, canvas.width, canvas.height)
+
+class Sprite {
+    constructor(position, velocity){
+        this.position = position
+        this.velocity = velocity
+    }
+
+    draw(){
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, 50, 150)
+    }
+}
+
+const player = new Sprite({x:0, y:0})
+const enemy = new Sprite({x:400, y:100})
+
+player.draw()
+enemy.draw()
+
+console.log(player)
+
+function animate() {
+    window.requestAnimationFrame(animate)
+}
+
+animate()
