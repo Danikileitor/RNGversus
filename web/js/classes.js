8fda84e034f32c8115098e6b9bdf57cab1fc1265
Danikileitor <focusyi@hotmail.com>
2022-12-18 13:59:52 +0100

    fondo animado

--- web/js/classes.js
+++ web/js/classes.js
@@ -1,70 +1,98 @@
 class Sprite {
-    constructor({position, imgSrc}){
-        this.position = position
-        this.width = 50
-        this.height = 150
-        this.image = new Image()
-        this.image.src = imgSrc
-    }
+  constructor({ position, imgSrc, scale = 1, framesMax = 1 }) {
+    this.position = position;
+    this.width = 50;
+    this.height = 150;
+    this.image = new Image();
+    this.image.src = imgSrc;
+    this.scale = scale;
+    this.framesMax = framesMax;
+    this.framesCurrent = 0;
+    this.framesElapsed = 0;
+    this.framesHold = 10;
+  }
 
-    draw(){
-        c.drawImage(this.image, this.position.x, this.position.y)
-    }
+  draw() {
+    c.drawImage(
+      this.image,
+      (this.framesCurrent * this.image.width) / this.framesMax,
+      0,
+      this.image.width / this.framesMax,
+      this.image.height,
+      this.position.x,
+      this.position.y,
+      (this.image.width / this.framesMax) * this.scale,
+      this.image.height * this.scale
+    );
+  }
 
-    update(){
-        this.draw()
+  update() {
+    this.draw();
+    this.framesElapsed++;
+    if (this.framesElapsed % this.framesHold === 0) {
+      if (this.framesCurrent < this.framesMax - 1) {
+        this.framesCurrent++;
+      } else {
+        this.framesCurrent = 0;
+      }
     }
+  }
 }
 
 class Luchador {
-    constructor({position, velocity, color, offset}){
-        this.position = position
-        this.velocity = velocity
-        this.width = 50
-        this.height = 150
-        this.lastKey
-        this.attackBox = {
-            position: {
-                x: this.position.x,
-                y: this.position.y
-            },
-            offset,
-            width: 100,
-            height: 50
-        }
-        this.color = color
-        this.isAttacking
-        this.health = 100
-    }
+  constructor({ position, velocity, color, offset }) {
+    this.position = position;
+    this.velocity = velocity;
+    this.width = 50;
+    this.height = 150;
+    this.lastKey;
+    this.attackBox = {
+      position: {
+        x: this.position.x,
+        y: this.position.y,
+      },
+      offset,
+      width: 100,
+      height: 50,
+    };
+    this.color = color;
+    this.isAttacking;
+    this.health = 100;
+  }
 
-    draw(){
-        //muñeco
-        c.fillStyle = this.color
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
-        //attackBox
-        if (this.isAttacking) {
-            c.fillStyle = 'green'
-            c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)   
-        }
+  draw() {
+    //muñeco
+    c.fillStyle = this.color;
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+    //attackBox
+    if (this.isAttacking) {
+      c.fillStyle = "green";
+      c.fillRect(
+        this.attackBox.position.x,
+        this.attackBox.position.y,
+        this.attackBox.width,
+        this.attackBox.height
+      );
     }
+  }
 
-    update(){
-        this.draw()
-        this.attackBox.position.x = this.position.x + this.attackBox.offset.x
-        this.attackBox.position.y = this.position.y
-        this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
-        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
-            this.velocity.y = 0
-        } else {
-            this.velocity.y += gravity
-        }
+  update() {
+    this.draw();
+    this.attackBox.position.x = this.position.x + this.attackBox.offset.x;
+    this.attackBox.position.y = this.position.y;
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+    if (this.position.y + this.height + this.velocity.y >= canvas.height - 96) {
+      this.velocity.y = 0;
+    } else {
+      this.velocity.y += gravity;
     }
+  }
 
-    attack(){
-        this.isAttacking = true
-        setTimeout(() => {
-            this.isAttacking = false
-        }, 100);
-    }
+  attack() {
+    this.isAttacking = true;
+    setTimeout(() => {
+      this.isAttacking = false;
+    }, 100);
+  }
 }
--- web/js/main.js
+++ web/js/main.js
@@ -1,179 +1,194 @@
-const canvas = document.getElementById('RNGversus')
-const c = canvas.getContext('2d')
+const canvas = document.getElementById("RNGversus");
+const c = canvas.getContext("2d");
 
-canvas.width = 1024
-canvas.height = 576
+canvas.width = 1024;
+canvas.height = 576;
 
-c.fillRect(0, 0, canvas.width, canvas.height)
+c.fillRect(0, 0, canvas.width, canvas.height);
 
-const gravity = 0.7
+const gravity = 0.7;
 const background = new Sprite({
-    position: {x: 0, y: 0},
-    imgSrc: './assets/oak_woods/background.png'
-})
+  position: { x: 0, y: 0 },
+  imgSrc: "./assets/oak_woods/background.png",
+});
+const shop = new Sprite({
+  position: { x: 625, y: 128 },
+  imgSrc: "./assets/oak_woods/shop.png",
+  scale: 2.75,
+  framesMax: 6,
+});
 
 const player1 = new Luchador({
-    position: {x:0, y:0},
-    velocity: {x:0, y:10},
-    color: 'red',
-    offset: {x:0, y:0}
-})
+  position: { x: 0, y: 0 },
+  velocity: { x: 0, y: 10 },
+  color: "red",
+  offset: { x: 0, y: 0 },
+});
 const player2 = new Luchador({
-    position: {x:400, y:100},
-    velocity: {x:0, y:10},
-    color: 'blue',
-    offset: {x:-50, y:0}
-})
+  position: { x: 400, y: 100 },
+  velocity: { x: 0, y: 10 },
+  color: "blue",
+  offset: { x: -50, y: 0 },
+});
 
-player1.draw()
-player2.draw()
+player1.draw();
+player2.draw();
 
-console.log(player1)
-console.log(player2)
+console.log(player1);
+console.log(player2);
 
 const keys = {
-    a: {pressed: false},
-    d: {pressed: false},
-    w: {pressed: false},
-    s: {pressed: false},
-    fa: {pressed: false},
-    fd: {pressed: false},
-    fw: {pressed: false},
-    fs: {pressed: false}
-}
+  a: { pressed: false },
+  d: { pressed: false },
+  w: { pressed: false },
+  s: { pressed: false },
+  fa: { pressed: false },
+  fd: { pressed: false },
+  fw: { pressed: false },
+  fs: { pressed: false },
+};
 
-reducirTimer()
+reducirTimer();
 
 function animate() {
-    window.requestAnimationFrame(animate)
-    c.fillStyle = 'black'
-    c.fillRect(0, 0, canvas.width, canvas.height)
-    background.update()
-    player1.update()
-    player2.update()
+  window.requestAnimationFrame(animate);
+  c.fillStyle = "black";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  background.update();
+  shop.update();
+  player1.update();
+  player2.update();
 
-    player1.velocity.x = 0
-    player2.velocity.x = 0
-    
-    //player1 movimiento
-    if (keys.a.pressed && player1.lastKey === 'a'){
-        player1.velocity.x = -5
-    } else if (keys.d.pressed && player1.lastKey === 'd'){
-        player1.velocity.x = 5
-    }
+  player1.velocity.x = 0;
+  player2.velocity.x = 0;
 
-    //player2 movimiento
-    if (keys.fa.pressed && player2.lastKey === 'fa'){
-        player2.velocity.x = -5
-    } else if (keys.fd.pressed && player2.lastKey === 'fd'){
-        player2.velocity.x = 5
-    }
+  //player1 movimiento
+  if (keys.a.pressed && player1.lastKey === "a") {
+    player1.velocity.x = -5;
+  } else if (keys.d.pressed && player1.lastKey === "d") {
+    player1.velocity.x = 5;
+  }
 
-    //detectar colisión
-    if (colisionRectangular({rectangulo1: player1, rectangulo2: player2}) && player1.isAttacking) {
-        player1.isAttacking = false
-        console.log('player1 hace daño a player2')
-        player2.health -= 10
-        document.getElementById('player2vidaInterna').style.width = player2.health + '%'
-    }
-    if (colisionRectangular({rectangulo1: player2, rectangulo2: player1}) && player2.isAttacking) {
-        player2.isAttacking = false
-        console.log('player2 hace daño a player1')
-        player1.health -= 10
-        document.getElementById('player1vidaInterna').style.width = player1.health + '%'
-    }
+  //player2 movimiento
+  if (keys.fa.pressed && player2.lastKey === "fa") {
+    player2.velocity.x = -5;
+  } else if (keys.fd.pressed && player2.lastKey === "fd") {
+    player2.velocity.x = 5;
+  }
 
-    //fin del juego por vida
-    if (player1.health <= 0 || player2.health <=0){
-        determinarGanador({player1, player2, timerId})
-    }
+  //detectar colisión
+  if (
+    colisionRectangular({ rectangulo1: player1, rectangulo2: player2 }) &&
+    player1.isAttacking
+  ) {
+    player1.isAttacking = false;
+    console.log("player1 hace daño a player2");
+    player2.health -= 10;
+    document.getElementById("player2vidaInterna").style.width =
+      player2.health + "%";
+  }
+  if (
+    colisionRectangular({ rectangulo1: player2, rectangulo2: player1 }) &&
+    player2.isAttacking
+  ) {
+    player2.isAttacking = false;
+    console.log("player2 hace daño a player1");
+    player1.health -= 10;
+    document.getElementById("player1vidaInterna").style.width =
+      player1.health + "%";
+  }
+
+  //fin del juego por vida
+  if (player1.health <= 0 || player2.health <= 0) {
+    determinarGanador({ player1, player2, timerId });
+  }
 }
 
-animate()
+animate();
 
-window.addEventListener('keydown', (event) => {
-    switch (event.code) {
-        //player1 movimiento
-        case 'KeyD':
-            keys.d.pressed = true
-            player1.lastKey = 'd'
-            break;
-        case 'KeyA':
-            keys.a.pressed = true
-            player1.lastKey = 'a'
-            break;
-        case 'KeyW':
-            keys.w.pressed = true
-            player1.lastKey = 'w'
-            player1.velocity.y = -20
-            break;
-        case 'KeyS':
-            keys.s.pressed = true
-            player1.lastKey = 's'
-            player1.velocity.y = 10
-            break;
-        //player1 ataques
-        case 'KeyJ':
-            player1.attack()
-            break;
-        //player2 movimiento
-        case 'ArrowRight':
-            keys.fd.pressed = true
-            player2.lastKey = 'fd'
-            break;
-        case 'ArrowLeft':
-            keys.fa.pressed = true
-            player2.lastKey = 'fa'
-            break;
-        case 'ArrowUp':
-            keys.fw.pressed = true
-            player2.lastKey = 'fw'
-            player2.velocity.y = -20
-            break;
-        case 'ArrowDown':
-            keys.fs.pressed = true
-            player2.lastKey = 'fs'
-            player2.velocity.y = 10
-            break;
-        //player2 movimiento
-        case 'Numpad1':
-            player2.attack()
-            break;
-        //default
-        default:
-            break;
-    }
-    console.log(event.code);
-})
-window.addEventListener('keyup', (event) => {
-    switch (event.code) {
-        case 'KeyD':
-            keys.d.pressed = false
-            break;
-        case 'KeyA':
-            keys.a.pressed = false
-            break;
-        case 'KeyW':
-            keys.w.pressed = false
-            break;
-        case 'KeyS':
-            keys.s.pressed = false
-            break;
-        
-        case 'ArrowRight':
-            keys.fd.pressed = false
-            break;
-        case 'ArrowLeft':
-            keys.fa.pressed = false
-            break;
-        case 'ArrowUp':
-            keys.fw.pressed = false
-            break;
-        case 'ArrowDown':
-            keys.fs.pressed = false
-            break;
-        
-        default:
-            break;
-    }
-})
+window.addEventListener("keydown", (event) => {
+  switch (event.code) {
+    //player1 movimiento
+    case "KeyD":
+      keys.d.pressed = true;
+      player1.lastKey = "d";
+      break;
+    case "KeyA":
+      keys.a.pressed = true;
+      player1.lastKey = "a";
+      break;
+    case "KeyW":
+      keys.w.pressed = true;
+      player1.lastKey = "w";
+      player1.velocity.y = -20;
+      break;
+    case "KeyS":
+      keys.s.pressed = true;
+      player1.lastKey = "s";
+      player1.velocity.y = 10;
+      break;
+    //player1 ataques
+    case "KeyJ":
+      player1.attack();
+      break;
+    //player2 movimiento
+    case "ArrowRight":
+      keys.fd.pressed = true;
+      player2.lastKey = "fd";
+      break;
+    case "ArrowLeft":
+      keys.fa.pressed = true;
+      player2.lastKey = "fa";
+      break;
+    case "ArrowUp":
+      keys.fw.pressed = true;
+      player2.lastKey = "fw";
+      player2.velocity.y = -20;
+      break;
+    case "ArrowDown":
+      keys.fs.pressed = true;
+      player2.lastKey = "fs";
+      player2.velocity.y = 10;
+      break;
+    //player2 movimiento
+    case "Numpad1":
+      player2.attack();
+      break;
+    //default
+    default:
+      break;
+  }
+  console.log(event.code);
+});
+window.addEventListener("keyup", (event) => {
+  switch (event.code) {
+    case "KeyD":
+      keys.d.pressed = false;
+      break;
+    case "KeyA":
+      keys.a.pressed = false;
+      break;
+    case "KeyW":
+      keys.w.pressed = false;
+      break;
+    case "KeyS":
+      keys.s.pressed = false;
+      break;
+
+    case "ArrowRight":
+      keys.fd.pressed = false;
+      break;
+    case "ArrowLeft":
+      keys.fa.pressed = false;
+      break;
+    case "ArrowUp":
+      keys.fw.pressed = false;
+      break;
+    case "ArrowDown":
+      keys.fs.pressed = false;
+      break;
+
+    default:
+      break;
+  }
+});
